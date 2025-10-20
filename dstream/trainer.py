56cb5754ba30b47be61afa9be7d5c76e7508ec02
Henry <sensork82@gmail.com>
2025-10-20 15:01:42 +0000

    flag the trainer

--- dstream/trainer.py
+++ dstream/trainer.py
@@ -1,13 +1,28 @@
-from transformers import Trainer 
+from transformers import Trainer, TrainingArguments 
+import torch.nn as nn
+
+class ScikitWrapper(nn.Module):
+
+  def __init__(self, model):
+    super(ScikitWrapper, self).__init__()
+    self.model = model
 
 class MLTrainer(Trainer):
-  def __init__(self, model=None, train_dataset=None):
-    self.model = model
-    self.train_dataset = train_dataset
+  
+  @classmethod
+  def from_scikit(cls, model, train_dataset=None, args: TrainingArguments = TrainingArguments(report_to='none')):
+    wrapmodel = ScikitWrapper(model)
+    return cls(model=wrapmodel,  train_dataset= train_dataset, args=args)
 
   def train(self):
-    features, target = self.train_dataset
-    self.model.fit(features, target)
+    if isinstance(self.train_dataset, Dataset):
+      features = self.train_dataset['input']
+      target = self.train_dataset['label']
+    else:
+      features, target = self.train_dataset
+
+    self.model.model.fit(features, target)
+
 
   def predict(self, X_test):
-    return self.model.predict(X_test)
+    return self.model.model.predict(X_test)
