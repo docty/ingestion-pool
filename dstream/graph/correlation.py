f5a4ab9c7cc6bda3bb6a9e561d4486acdb0b0881
Henry <sensork82@gmail.com>
2025-10-22 20:49:36 +0000

    correlation

---
+++ dstream/graph/correlation.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+from .base import BasePlot
+
+class CorrelationPlot(BasePlot):
+    """correlation visualization."""
+    
+    def plot(self, data, annot=True):
+        ax = self._init_ax()
+        sns.heatmap(data.corr(), annot=annot, cmap="coolwarm", ax=ax)
+        ax.set_title("Correlation Matrix")
+        self._save_or_show()
+
