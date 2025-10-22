3b7fb5944638e36c52a682b1b31af9f491999e08
Henry <sensork82@gmail.com>
2025-10-22 01:39:23 +0000

    fix the scaler and feature

--- dstream/preprocess/__init__.py
+++ dstream/preprocess/__init__.py
@@ -1,13 +1,14 @@
 from .pipeline import DataPreprocessor
 from .imputer import DataImputer
 from .encoder import DataEncoder
-from .encoder import DataScaler
+from .scaler import DataScaler
 from .splitter import SimpleDataSplitter
-
+from .selector import FeatureSelector
 __all__ = [
     'DataPreprocessor',
     'DataImputer',
     'DataEncoder',
     'DataScaler',
-    'SimpleDataSplitter'
+    'SimpleDataSplitter',
+    'FeatureSelector'
 ]
