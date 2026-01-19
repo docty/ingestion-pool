bf07ee333f0293c9095cf3a1c0ab6cc819196b8a
Henry <synevix@gmail.com>
2026-01-19 23:28:10 +0000

    fix the conflict utils

--- dstream/models/trainargs.py
+++ dstream/models/trainargs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from dataclasses import dataclass
 
 
@@ -8,15 +7,4 @@ class TrainingArguments:
     report_to="none"
 
 
-=======
-from dataclasses import dataclass
-
-
-@dataclass
-class TrainingArguments:
-    output_dir="./output"
-    report_to="none"
-
-
->>>>>>> 058324c3910f6b2c6fb807b667322e642be77b2a
  
